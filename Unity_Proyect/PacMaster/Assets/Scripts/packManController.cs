f91c3da284863c0cb4f8f2a391260f343bca6e6e
Joel <joelps117@hotmail.es>
2022-08-10 00:18:34 +0200

    TODO check move pacman

--- Unity_Proyect/PacMaster/Assets/Scripts/packManController.cs
+++ Unity_Proyect/PacMaster/Assets/Scripts/packManController.cs
@@ -7,8 +7,13 @@ public class packManController : MonoBehaviour
     public GameObject pacman;
     private float horizontalMove;
     private float verticalMove;
-    public CharacterController pacManChCon;
+    private CharacterController pacManChCon;
     public float packManSpeed;
+    private Vector3 playerInput;
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+    private Vector3 movePlayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +25,23 @@ public class packManController : MonoBehaviour
     {
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
-    }
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+        camDirection();
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+        pacManChCon.transform.LookAt(pacman.transform.position + movePlayer);
+        pacManChCon.Move(movePlayer * packManSpeed * Time.deltaTime);
 
-    private void FixedUpdate()
+
+    }
+    void camDirection()
     {
-        Vector3 move = pacman.transform.right * horizontalMove + pacman.transform.forward * verticalMove;
-        pacManChCon.Move(move * packManSpeed * Time.deltaTime);
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
     }
 }
